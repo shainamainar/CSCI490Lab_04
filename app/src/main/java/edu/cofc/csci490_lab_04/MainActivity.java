05f5b2b78524f1f52c4e9e8a1bc03502ebbfaca5
delacruzkb <delacruzkb@g.cofc.edu>
2018-03-08 17:45:32 -0500

    Initial commit

---
+++ app/src/main/java/edu/cofc/csci490_lab_04/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.cofc.csci490_lab_04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
